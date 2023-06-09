21c0c778669e20eef27e4041be3a64e48d599e24
Rokas <rookas.rudzenskas@gmail.com>
2023-06-09 17:05:15 +0300

    ✨ feat(CarCard.tsx): add car image and additional car details to improve user experience and provide more information about the car.

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -2,6 +2,8 @@
 import React, {useState} from 'react';
 import {CarProps} from "@/types";
 import {calculateCarRent} from "@/utils";
+import Image from "next/image";
+import {CustomButton} from "@/components";
 
 interface CarCardProps {
   car: CarProps;
@@ -24,7 +26,48 @@ const CarCard = ({car}: CarCardProps) => {
         {carRent}
         <span className='self-end text-[14px] leading-[17px] font-medium'>/day</span>
       </p>
+
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image
+          alt='car model'
+          fill
+          priority
+          className='object-contain'
+          src="/hero.png"
+        />
+      </div>
+
+      <div className='relative flex w-full mt-2'>
+      <div className='flex group-hover:invisible w-full justify-between text-grey'>
+        <div className='flex flex-col justify-center items-center gap-2'>
+          <Image src='/steering-wheel.svg' width={20} height={20} alt='steering wheel' />
+          <p className='text-[14px] leading-[17px]'>
+            {transmission === "a" ? "Automatic" : "Manual"}
+          </p>
+        </div>
+        <div className="car-card__icon">
+          <Image src="/tire.svg" width={20} height={20} alt="seat" />
+          <p className="car-card__icon-text">{drive.toUpperCase()}</p>
+        </div>
+        <div className="car-card__icon">
+          <Image src="/gas.svg" width={20} height={20} alt="seat" />
+          <p className="car-card__icon-text">{city_mpg} MPG</p>
+        </div>
+      </div>
+
+      <div className="car-card__btn-container">
+        <CustomButton
+          title='View More'
+          containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
+          textStyles='text-white text-[14px] leading-[17px] font-bold'
+          rightIcon='/right-arrow.svg'
+          handleClick={() => setIsOpen(true)}
+        />
+      </div>
     </div>
+
+
+</div>
   );
 };
 
