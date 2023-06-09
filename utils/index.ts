2f26725808d387c70ea1e155e4d359e84fcb1bd2
Rokas <rookas.rudzenskas@gmail.com>
2023-06-09 16:45:28 +0300

    🔥 refactor(utils/index.ts): remove unnecessary comments and variable destructuring 👌 refactor(utils/index.ts): remove unused variable 'headers' and parse response as JSON in the same line

--- utils/index.ts
+++ utils/index.ts
@@ -3,13 +3,11 @@ import {FilterProps} from "@/types";
 export async function fetchCars(filters: FilterProps) {
   const { manufacturer, year, model, limit, fuel } = filters;
 
-  // Set the required headers for the API request
   const headers: HeadersInit = {
     "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
   };
 
-  // Set the required headers for the API request
   const response = await fetch(
     `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
     {
@@ -17,8 +15,6 @@ export async function fetchCars(filters: FilterProps) {
     }
   );
 
-  // Parse the response as JSON
   const result = await response.json();
-
   return result;
 }
