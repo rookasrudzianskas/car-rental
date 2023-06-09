5970ab03194f726e616f116309d18d38b4078f61
Rokas <rookas.rudzenskas@gmail.com>
2023-06-09 16:39:42 +0300

    ✨ feat(SearchBar.tsx): add input field for car model search to improve search functionality

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -34,6 +34,24 @@ const SearchBar = ({}) => {
         />
         <SearchButton otherClasses='sm:hidden' />
       </div>
+      <div className='searchbar__item'>
+        <Image
+          src='/model-icon.png'
+          width={25}
+          height={25}
+          className='absolute w-[20px] h-[20px] ml-4'
+          alt='car model'
+        />
+        <input
+          type='text'
+          name='model'
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder='Tiguan...'
+          className='searchbar__input'
+        />
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
     </form>
   );
 };
