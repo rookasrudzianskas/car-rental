c5ee6a574076fb2477e6a20ae55aaa352b74e1d4
Rokas <rookas.rudzenskas@gmail.com>
2023-06-09 16:30:10 +0300

    ♻️ refactor(SearchManufacturer.tsx): change import path of manufacturers constant to use absolute path 🔥 chore(SearchManufacturer.tsx): remove unused import statement to improve code readability

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 import { Fragment, useState } from "react";
 import { Combobox, Transition } from "@headlessui/react";
 
-import { manufacturers } from "@constants";
 import { SearchManuFacturerProps } from "@types";
+import {manufacturers} from "@/constants";
 
 const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacturerProps) => {
   const [query, setQuery] = useState("");
