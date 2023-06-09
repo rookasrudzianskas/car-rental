13f101ef914943d71673344e94f105a62d1b8bd0
Rokas <rookas.rudzenskas@gmail.com>
2023-06-09 16:31:20 +0300

    🔥 refactor(SearchManufacturer.tsx): remove comment that explains the obvious

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -37,7 +37,7 @@ const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacture
           <Combobox.Input
             className='search-manufacturer__input'
             displayValue={(item: string) => item}
-            onChange={(event) => setQuery(event.target.value)} // Update the search query when the input changes
+            onChange={(event) => setQuery(event.target.value)}
             placeholder='Volkswagen...'
           />
 
