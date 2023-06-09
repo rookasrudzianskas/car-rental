7c21c6e61ab5bb3c59d5ec94ecf982371ba8cba3
Rokas <rookas.rudzenskas@gmail.com>
2023-06-09 16:47:30 +0300

    🚀 feat(page.tsx): add async function to fetch cars with search parameters to improve performance and user experience 🎨 style(page.tsx): remove unnecessary spaces and add missing semicolons to improve code readability and consistency

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,32 @@
 import Image from 'next/image'
 import React from "react";
 import {CustomFilter, Hero, SearchBar} from "@/components";
+import {fetchCars} from "@/utils";
+import {HomeProps} from "@/types";
 
-export default function Home() {
+export default async function Home({searchParams}: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
+
+
   return (
     <main className="overflow-hidden">
-      <Hero />
+      <Hero/>
       <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
           <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
           <p>Explore the cars you might like</p>
         </div>
         <div className="home__filters">
-          <SearchBar />
+          <SearchBar/>
           <div className="home__filter-container">
-            <CustomFilter title="Fuel" />
-            <CustomFilter title="Year" />
+            <CustomFilter title="Fuel"/>
+            <CustomFilter title="Year"/>
           </div>
         </div>
       </div>
