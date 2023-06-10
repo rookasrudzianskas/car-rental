e7c67755914c9b4c80bc84d916bf91a4b1540340
Rokas <rookas.rudzenskas@gmail.com>
2023-06-10 09:03:45 +0300

    🎨 style(CarDetails.tsx): improve layout and readability of car details modal by adjusting flexbox properties and adding more spacing between elements ✨ feat(CarDetails.tsx): add dynamic rendering of car details by iterating over object entries and displaying key-value pairs in a more readable format

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -52,7 +52,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                     />
                   </button>
 
-                  <div className="flex flex-1 flex-col gap-3">
+                  <div className='flex-1 flex flex-col gap-3'>
                     <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg'>
                       <Image src={generateCarImageUrl(car)} alt='car model' fill priority className='object-contain' />
                     </div>
@@ -67,6 +67,25 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                       <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
                         <Image  src={generateCarImageUrl(car, "13")} alt='car model' fill priority className='object-contain' />
                       </div>
+                    </div>
+                  </div>
+
+                  <div className='flex-1 flex flex-col gap-2'>
+                    <h2 className='font-semibold text-xl capitalize'>
+                      {car.make} {car.model}
+                    </h2>
+
+                    <div className='mt-3 flex flex-wrap gap-4'>
+                      {Object.entries(car).map(([key, value]) => (
+                        <div className='flex justify-between gap-5 w-full text-right' key={key} >
+                          <h4 className='text-grey capitalize'>
+                            {key.split("_").join(" ")}
+                          </h4>
+                          <p className='text-black-100 font-semibold'>
+                            {value}
+                          </p>
+                        </div>
+                      ))}
                     </div>
                   </div>
                 </Dialog.Panel>
