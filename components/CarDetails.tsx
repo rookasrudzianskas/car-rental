e9edc7289130be4cc29103072a1e0e886d0da830
Rokas <rookas.rudzenskas@gmail.com>
2023-06-10 09:02:56 +0300

    🎨 style(CarDetails.tsx): add close button to the car details modal for better user experience 🚀 perf(CarDetails.tsx): add next/image component to improve image loading performance in the car details modal 🔥 chore(CarDetails.tsx): remove unused imports and add alt text to car images in the car details modal

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -2,6 +2,7 @@
 import React, {Fragment} from 'react';
 import {CarProps} from "@/types";
 import {Dialog, Transition} from "@headlessui/react";
+import Image from "next/image";
 
 interface CarDetailsProps {
   isOpen: boolean;
@@ -38,7 +39,36 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                 leaveTo='opacity-0 scale-95'
               >
                 <Dialog.Panel className='relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5'>
+                  <button
+                    type='button'
+                    className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full"
+                  >
+                    <Image
+                      src='/close.svg'
+                      alt='close'
+                      width={20}
+                      height={20}
+                      className='object-contain'
+                    />
+                  </button>
 
+                  <div className="flex flex-1 flex-col gap-3">
+                    <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg'>
+                      <Image src={generateCarImageUrl(car)} alt='car model' fill priority className='object-contain' />
+                    </div>
+
+                    <div className='flex gap-3'>
+                      <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                        <Image src={generateCarImageUrl(car, "29")} alt='car model' fill priority className='object-contain' />
+                      </div>
+                      <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                        <Image src={generateCarImageUrl(car, "33")} alt='car model' fill priority className='object-contain' />
+                      </div>
+                      <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                        <Image  src={generateCarImageUrl(car, "13")} alt='car model' fill priority className='object-contain' />
+                      </div>
+                    </div>
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
