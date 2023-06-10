8997320f59ebd9800766e9f9eee50b27b20a3127
Rokas <rookas.rudzenskas@gmail.com>
2023-06-10 10:04:57 +0300

    ✨ feat(page.tsx): add ShowMore component to display a button to load more cars 🎨 style(ShowMore.tsx): add styles to ShowMore component and change button text to "Show More" to improve UX 🐛 fix(ShowMore.tsx): fix handleNavigation function to update limit search param correctly when clicking on Show More button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import React from "react";
-import {CarCard, CustomFilter, Hero, SearchBar} from "@/components";
+import {CarCard, CustomFilter, Hero, SearchBar, ShowMore} from "@/components";
 import {fetchCars} from "@/utils";
 import {HomeProps} from "@/types";
 import {fuels, yearsOfProduction} from "@/constants";
@@ -40,10 +40,10 @@ export default async function Home({searchParams}: HomeProps) {
               ))}
             </div>
 
-            {/*<ShowMore*/}
-            {/*  pageNumber={(searchParams.limit || 10) / 10}*/}
-            {/*  isNext={(searchParams.limit || 10) > allCars.length}*/}
-            {/*/>*/}
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
           </section>
         ) : (
           <div className='home__error-container'>
--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,9 +1,30 @@
+"use client";
 import React from 'react';
+import {ShowMoreProps} from "@/types";
+import {useRouter} from "next/navigation";
+import {updateSearchParams} from "@/utils";
+import {CustomButton} from "@/components";
 
-const ShowMore = ({}) => {
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+    const newPathname = updateSearchParams("limit", `${newLimit}`);
+
+    router.push(newPathname);
+  };
+
   return (
-    <div>
-      
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          btnType="button"
+          title="Show More"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
     </div>
   );
 };
